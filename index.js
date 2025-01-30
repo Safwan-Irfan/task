5ab69b24269ac85bc43bdf916b0dda61b39cfaf1
ImSafwan <safwanirfan221@gmail.com>
2025-01-30 23:52:09 +0500

    Commiting file

---
+++ index.js
@@ -0,0 +1,25 @@
+// var name = "Safwan"
+// console.log(name)
+// console.log(typeof(name))
+// 
+// var age = "16"
+// console.log(age)
+// 
+// 
+// var number = 132456789
+// console.log(number)
+// 
+// var  attendence  = present = true
+// console.log(attendence)
+
+
+// var age = 16
+// var msg = ("age >=16")? true("Youre Teenager") false("Youre Young")
+// {
+    // console.log("age")
+// }
+
+var age = prompt("Enter your age")
+var msg = (age <= 18 ) ? "You're Young" : "You're Old";
+
+alert(msg);
